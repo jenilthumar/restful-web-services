ee0f9e238e9d757b7e54fc5d119d6c4e35b0a0f8
Jenil Thummar <108112410+jenilthumar@users.noreply.github.com>
2023-07-11 11:25:29 +0530

    Implemented OpenAPI docs for API

--- src/main/java/com/jenil/learning/webservices/restfulwebservices/RestfulWebServicesApplication.java
+++ src/main/java/com/jenil/learning/webservices/restfulwebservices/RestfulWebServicesApplication.java
@@ -6,10 +6,8 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.support.ResourceBundleMessageSource;
 import org.springframework.web.servlet.LocaleResolver;
 import org.springframework.web.servlet.i18n.AcceptHeaderLocaleResolver;
-import org.springframework.web.servlet.i18n.SessionLocaleResolver;
 
 import java.util.Locale;
-
 @SpringBootApplication
 public class RestfulWebServicesApplication {
 
