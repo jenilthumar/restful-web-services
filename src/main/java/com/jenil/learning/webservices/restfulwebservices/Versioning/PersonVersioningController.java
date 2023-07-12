6c30ed467db3c633fa940c18bc7893eef91efdad
Jenil Thummar <108112410+jenilthumar@users.noreply.github.com>
2023-07-12 19:41:28 +0530

    modified versioning, changed from URI versioning to request parameter

--- src/main/java/com/jenil/learning/webservices/restfulwebservices/Versioning/PersonVersioningController.java
+++ src/main/java/com/jenil/learning/webservices/restfulwebservices/Versioning/PersonVersioningController.java
@@ -6,13 +6,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class PersonVersioningController {
 
-    @GetMapping("v1/person")
-    public PersonV1 personV1(){
+    @GetMapping(value="/person/param", params = "version=1")
+    public PersonV1 paramV1(){
         return new PersonV1("Jenil Thummar");
     }
 
-    @GetMapping("v2/person")
-    public PersonV2 personV2() {
+    @GetMapping(value="/person/param", params = "version=2")
+    public PersonV2 paramV2() {
         return new PersonV2(new Name("Jenil", "Thummar"));
     }
 
