ddb5fd6af08909d9748bd63b1c40726b4112bef3
Jenil Thummar <108112410+jenilthumar@users.noreply.github.com>
2023-07-13 19:32:11 +0530

    Connected to JPA

---
+++ src/main/resources/data.sql
@@ -0,0 +1,6 @@
+insert into customer values('2022-1-1', 1, 'Jenil');
+insert into customer values('2022-2-1', 2, 'Vraj');
+insert into customer values('2022-3-1', 3, 'Venil');
+insert into customer values('2022-4-1', 4, 'Vasu');
+insert into customer values('2022-5-1', 5, 'Lathiya');
+insert into customer values('2022-6-1', 6, 'Anand');
